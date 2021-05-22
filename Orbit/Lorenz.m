892e3d461f1ccf20d8013d0ea35623cc2873a300
HeoSe0 <heose0@yu.ac.kr>
2021-05-22 21:46:14 +0900

    Add Lorenz.m

---
+++ Orbit/Lorenz.m
@@ -0,0 +1,62 @@
+epoch = 1000;
+tRange = linspace(0, 30, epoch);
+x = zeros(1000, epoch); y = zeros(1000, epoch); z = zeros(1000, epoch);
+
+for i = 1:epoch
+    randY0 = [(-1)^randi(2) * rand * 3, (-1)^randi(2) * rand * 3, (-1)^randi(2) * rand * 3];
+    [tSol, YSol] = ode45(@lorenz, tRange, randY0);
+    x(:, i) = YSol(:, 1); y(:, i) = YSol(:, 2); z(:, i) = YSol(:, 3);
+end
+
+figure(1)
+
+for i = 1:epoch
+    plot(x(:, i), z(:, i));
+    hold on;
+end
+
+xlabel('x-position');
+ylabel('z-position');
+axis equal;
+hold off;
+
+figure(2)
+subplot(1, 3, 1);
+
+for i = 1:epoch
+    plot(tSol, x(:, i));
+    hold on;
+end
+
+xlabel('time');
+ylabel('x-position');
+hold off;
+subplot(1, 3, 2);
+
+for i = 1:epoch
+    plot(tSol, y(:, i));
+    hold on;
+end
+
+xlabel('time');
+ylabel('y-position');
+hold off;
+subplot(1, 3, 3);
+
+for i = 1:epoch
+    plot(tSol, z(:, i));
+    hold on;
+end
+
+xlabel('time');
+ylabel('z-position');
+hold off;
+
+function dYdt = lorenz(t, Y)
+    x = Y(1); y = Y(2); z = Y(3);
+    sigma = 10; beta = 8/3; rho = 1.2;
+    dxdt = sigma * (y - x);
+    dydt = x * (rho - z) - y;
+    dzdt = x * y - beta * z;
+    dYdt = [dxdt; dydt; dzdt];
+end
